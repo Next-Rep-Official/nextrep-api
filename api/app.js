b6e68791af155bb666038f897aaf37629528c01c
Zan e1 <zbeidas@icloud.com>
2026-02-14 22:55:41 -0500

    Removed rate limiter only for TESTING

--- api/app.js
+++ api/app.js
@@ -35,14 +35,14 @@ Object.entries(config).forEach(([sectionKey, sectionValue]) => {
 app.use(express.json());
 app.use(cors());
 
-// Handle rate limiting
-const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 15 minutes
-    max: 300, // Limit each IP to 100 requests per `windowMs`
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-app.use(limiter);
+// // Handle rate limiting
+// const limiter = rateLimit({
+//     windowMs: 10 * 60 * 1000, // 15 minutes
+//     max: 300, // Limit each IP to 100 requests per `windowMs`
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
+// app.use(limiter);
 
 // Add Tables
 await initTables().then((value) => console.log(value));
